0b2c9ff358ca53ea8cfa856b3ce7b38fab758ae6
Deepanshu <deepupant2002@gmail.com>
2023-05-29 02:14:43 +0530

    Add project files.

---
+++ Bulky.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,25 @@
+﻿using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private ApplicationDBContext _db;
+        public CategoryRepository(ApplicationDBContext category) : base(category)
+        {
+            _db = category;
+        }
+
+        public void Update(Category obj)
+        {
+            _db.Categories.Update(obj);
+        }
+    }
+}
---
+++ WebApplication1/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BulkyBook.Models;
+using BulkyBook.DataAccess.Repository.IRepository;
+
+namespace BulkyBookWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public CategoryController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _unitOfWork.Category.GetAll().ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Category.Add(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Category Created Successfully";
+
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Edit(int? Id)
+        {
+            if (Id == 0 || Id == null)
+            {
+                return NotFound();
+            }
+            Category? fromdb = _unitOfWork.Category.Get(u => u.Id == Id);
+            if (fromdb == null) { return NotFound(); }
+            return View(fromdb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Category.Update(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Category Updated Successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int? Id)
+        {
+            if (Id == 0 || Id == null)
+            {
+                return NotFound();
+            }
+            Category fromdb = _unitOfWork.Category.Get(u => u.Id == Id);
+            if (fromdb == null) { return NotFound(); }
+            return View(fromdb);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? Id)
+        {
+            Category? finddb = _unitOfWork.Category.Get(u => u.Id == Id);
+            if (finddb == null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.Category.Remove(finddb);
+            _unitOfWork.Save();
+            TempData["success"] = "Category Deleted Successfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
